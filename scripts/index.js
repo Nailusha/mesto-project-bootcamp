8ba2bb061c47a519de271c99d313f1afa2c17f27
Nailya Yakimova <yakimovanailusha@yandex.ru>
2023-04-10 18:43:40 +0300

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const add = document.querySelector('.popup__card'); // получаем модификатор попапа для добавления
 const edit = document.querySelector('.popup__profile'); // получаем модификатор попапа для редактирвоания
+const cardConteiner = document.querySelector('.element');
 
 let closePopupButton = []; // кнопка для закрытия окна
 closePopupButton = document.querySelectorAll('.popup__button-closed'); // получаем массив всех кнопок closed
@@ -25,22 +26,8 @@ buttonAdd.addEventListener('click', function () {
     add.classList.add('popup__opened');
 });
 
-// Находим контейнер, в который будем добавлять карточки
-const container = document.querySelector('.card-image');
 
-// Находим шаблон карточки
-const cardTemplate = document.querySelector('#card-template');
-
-// Создаем новый узел на основе шаблона
-const cardNode = cardTemplate.content.cloneNode(true);
-
-// Находим элементы карточки внутри созданного узла
-const cardImage = cardNode.querySelector('.element__list-item');
-const cardTitle = cardNode.querySelector('.element__title');
-const likeButton = cardNode.querySelector('.element__button-like');
-const imageContainer = document.getElementById('.elements__list');
-
-const сards = [
+const сardsImage = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -67,45 +54,47 @@ const сards = [
   }
 ];
 
-function createCard(data) {
+function createCardElement(cardData) {
+  
+  // Находим шаблон карточки
+  const cardTemplate = document.querySelector('#card-template');
 
-  cardImage.src = data.link;
-  cardTitle.textContent = data.name;
+  // Создаем новый узел на основе шаблона
+  const cardNode = cardTemplate.content.cloneNode(true);
+
+  // Находим элементы карточки внутри созданного узла
+  const cardImage = cardNode.querySelector('.element__list-item');
+  cardImage.src = cardData.link;
+
+  const cardTitle = cardNode.querySelector('.element__title');
+  cardTitle.textContent = cardData.name;
+
+  const likeButton = cardNode.querySelector('.element__button-like');
+  // Добавляем слушатели событий для кнопок
+  let clicked = false;
+  likeButton.addEventListener ('click', function() {
+      if (!clicked) {
+        likeButton.style.backgroundColor = 'black';
+          clicked = true;
+      } else {
+        likeButton.style.backgroundColor = '';
+          clicked = false;
+      }
+  });
+  const deleteButton = cardNode.querySelector('.element__button-delete');
+    for (let i = 0; i < deleteButton.length; i++) {
+      deleteButton[i].addEventListener('click', function() {
+        cardImage[i].remove();
+    });
+    };
+
+  // Добавляем созданную карточку в контейнер
+  container.appendChild(cardNode);
 
   return cardNode;
-}
+};
 
-buttonAdd.addEventListener('click', function() {
-  const newCard = createCard(cards);
-  container.appendChild(newCard);
-});
 
-// Добавляем созданную карточку в контейнер
-container.addEventListener('click', () => {
-    const container = document.createElement('.ul');
-    container.classList.add('.card-image');
-    container.innerHTML = `
-      <h3>Заголовок карточки</h3>
-      <p>Описание карточки</p>
-    `;
-    imageContainer.appendChild(cardNode);
-});
 
-// Добавляем слушатели событий для кнопок
-let clicked = false;
-likeButton.addEventListener ('click', function() {
-    if (!clicked) {
-      likeButton.style.backgroundColor = 'black';
-        clicked = true;
-    } else {
-      likeButton.style.backgroundColor = '';
-        clicked = false;
-    }
-});
 
-const deleteButton = cardNode.querySelector('.element__button-delete');
-  for (let i = 0; i < deleteButton.length; i++) {
-    deleteButton[i].addEventListener('click', function() {
-      cardImage[i].remove();
-  });
-  };
+

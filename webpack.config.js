d9c7e74a52db4aee6d1f31ac64848077a5bfaecb
Nailya Yakimova <yakimovanailusha@yandex.ru>
2023-06-15 23:09:36 +0300

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключите плагин 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
 // подключите к проекту mini-css-extract-plugin
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
+  entry: { main: './src/index.js' },
+  output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-        publicPath: ''
+    publicPath: ''
   },
-    mode: 'development',
+  mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
     compress: true,
     port: 8080,
     open: true
   },
-    module: {
+  module: {
     rules: [ // rules — это массив правил
       // добавим в него объект правил для бабеля
       {
@@ -37,13 +37,17 @@ module.exports = {
       },
       // добавьте ещё одно правило:
       {
-      // применять это правило только к CSS-файлам
-      test: /\.css$/,
-      // при обработке этих файлов нужно использовать
-      // MiniCssExtractPlugin.loader и css-loader
-      use: [MiniCssExtractPlugin.loader, {
-        loader: 'css-loader'
-      }]
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          // добавьте объект options
+        options: { importLoaders: 1 }
+        },
+    // Добавьте postcss-loader
+        'postcss-loader']
       }
     ]
   },
